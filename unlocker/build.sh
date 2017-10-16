4c57782a8fd6020cbfa76e5a696bafcbe68afb56
Hleb Skuratau <Hleb_Skuratau@epam.com>
2017-10-16 16:53:15 +0300

    Update build.sh

--- unlocker/build.sh
+++ unlocker/build.sh
@@ -1,3 +1,3 @@
-IMAGE=`docker build . | grep Successfully | awk '{print $3}'`
-IMAGE=`docker build . | grep Successfully | awk '{print $3}'`
+LAST_STRING=`docker build . | tail -n 1`
+IMAGE=`echo $LAST_STRING | grep Successfully | awk '{print $3}'`
 docker tag $IMAGE unlocker:latest
